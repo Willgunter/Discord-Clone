3f8ce68b3418e841877240a37aa057e65224227c
Willgunter <jameswgunterlll@gmail.com>
2024-01-19 22:28:09 -0500

    almost ready to have add server feature

--- Angular Frontend/src/app/components/data-columns/server-column/server-column.component.ts
+++ Angular Frontend/src/app/components/data-columns/server-column/server-column.component.ts
@@ -41,49 +41,36 @@ export class ServerColumnComponent {
     }
 
     onSubmit(form: NgForm) {
-        
-            // parsing current route string
-        //     this.server = this.currentRoute.substring(1).split("/", 2)[0];
-        //     this.channel = this.currentRoute.substring(1).split("/", 2)[1];
     
             if(!this.name) {
                 alert('Please add a name for your server');
                 return;
             }
-    
-        //     // Constructing a new message object with text, server, and channel values
+
+            // CURRENT TASK
+            // something like this probably??
+            // const files: File[] = Array.from(form.value.files).filter(file => file.type === 'image/png');
+        
+
+            // Constructing a new Server object
             const newServer: Server = {
                 name: this.name,
-                // channels: [
-                //     { name: "general", messages: [] },
-                //     { name: "welcome", messages: [] },
-                //     { name: "rules", messages: [] },
-                // ],
+                
             };
     
-        //     // assigning form values to server and channel values inherited from app-main
             form.value.name = this.name;
         //     // postMessage(newMessage) kind of works but somehow gives an error as well???
             this.configService.postServer(form.value).subscribe((res) => {
                 // CURRENTL PROBLEM
                 console.log("test");
             });
-            
-            // this.onAddMessage.emit(newMessage); // I don't know what this is but code seems to work find without it
     
             this.configService.getServerList().subscribe((res) => {
                 this.resetForm(form);
                 this.refreshServerList();
-        //         // at some point I would like to either make this in a better spot
-        //         // or I may even just get rid of it altogether
                 M.toast({html: 'Server Created', classes: 'rounded'});
             });
-        // // is this where "defaultserver" is happening?
-        // // does this even do anything?????
-        
-    
-        //     // vvv does this do anything vvv
-        //     // I think it might reset the text value?\
+
         this.showBox = false;
     
       
